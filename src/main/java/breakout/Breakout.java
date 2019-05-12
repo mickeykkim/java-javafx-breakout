e42c192148bc361a45665f60ee4e09922f442a5e
Mickey K <mickeykkim@gmail.com>
2019-05-12 17:17:03 +0100

    Finished 1.0

--- src/main/java/breakout/Breakout.java
+++ src/main/java/breakout/Breakout.java
@@ -29,70 +29,68 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class Breakout extends Application {
-   private final int FPS60MS  = 16;
-   private final int WIDTH    = 600;
-   private final int HEIGHT   = 900;
-   private final int PWIDTH   = 100;
-   private final int PHEIGHT  = 15;
-   private final int BWIDTH   = 70;
-   private final int BHEIGHT  = 20;
-   private final int BRICK_XOFFSET = 10;
-   private final int BRICK_YOFFSET = 70;
-   private final int BRICK_XGAP    = 85;
-   private final int BRICK_YGAP    = 30;
-   private final double BALL_RADIUS  = 10;
-   private final double BALL_SPEED   = 6;
-   private final double PADDLE_SPEED = 8;
-   private final double PADDLE_XMOD  = 0.8;
-   private boolean roundHasStarted = false;
+   private static final int FPS60MS  = 16;
+   private static final int WIDTH    = 600;
+   private static final int HEIGHT   = 900;
+   private static final int PWIDTH   = 100;
+   private static final int PHEIGHT  = 15;
+   private static final int BWIDTH   = 70;
+   private static final int BHEIGHT  = 20;
+   private static final int LABELPADD = 40;
+   private static final int LABELX    = 130;
+   private static final int LABELY    = 400;
+   private static final int BRICK_XOFFSET = 10;
+   private static final int BRICK_YOFFSET = 70;
+   private static final int BRICK_XGAP    = 85;
+   private static final int BRICK_YGAP    = 30;
+   private static final double BALL_RADIUS  = 10;
+   private static final double BALL_SPEED   = 6;
+   private static final double PADDLE_SPEED = 8;
+   private static final double PADDLE_XMOD  = 0.8;
+   // Free sounds downloaded from : https://www.noiseforfun.com
+   // Win sound from : https://archive.org/details/FF7ACVictoryFanfareRingtoneperfectedMp3
+   private final String PADDLEHIT = "audio/paddle_hit.wav";
+   private final String BRICKHIT  = "audio/brick_hit.wav";
+   private final String DIED      = "audio/died.wav";
+   private final String GAMEOVER  = "audio/gameover.wav";
+   private final String GAMEWIN   = "audio/win.wav";
+   private final String BGIMG     = "image/bg_stars.png";
    private Color brickColors[] = {Color.RED, Color.ORANGE, 
                                   Color.YELLOW, Color.GREEN, Color.BLUE, 
                                   Color.MAGENTA, Color.PURPLE};
-   // Free sounds downloaded from : https://www.noiseforfun.com
-   // Win sound from : https://archive.org/details/FF7ACVictoryFanfareRingtoneperfectedMp3
-   private final String paddleHit = "audio/paddle_hit.wav";
-   private final String brickHit  = "audio/brick_hit.wav";
-   private final String died      = "audio/died.wav";
-   private final String gameover  = "audio/gameover.wav";
-   private final String gamewin   = "audio/win.wav";
-   private final String bgImg     = "image/bg_stars.png";
-
    private List<Brick> brickList = new ArrayList<Brick>();
-   private Ball gameBall;
-   private Paddle gamePaddle;
-   private StackPane splash = new StackPane();
-   private GridPane topText = new GridPane();
-   private int playerLives = 3;
+   private boolean roundHasStarted = false;
+   private int playerLives  = 3;
    private int currentLevel = 1;
    private int currentScore = 0;
-   private Group root;
-
    private final Label labelStart = new Label(
       "Breakout!\n\nPress Space to start a new game" +
       "\nMove with arrow keys or A and D\nPress ESC to exit game");
    private final Label labelGameover = new Label(
-      "Game Over\n\n" + "Your score: " + currentScore +
+      "Game Over\n\n" +
       "\nPress Space to start a new game" +
       "\nPress ESC to exit game");
    private final Label labelGamewon = new Label(
       "Congratulations You Won!\n" +
-      "\nPress Space to continue to level " + currentLevel +
+      "\nPress Space to continue" +
       "\nPress ESC to exit game");
-   private static final int LABELPADD = 40;
-   private static final int LABELX = 130;
-   private static final int LABELY = 400;
+   private StackPane splash = new StackPane();
+   private GridPane topText = new GridPane();
+   private Paddle gamePaddle;
+   private Ball gameBall;
+   private Group root;
 
    @Override
    public void start(Stage stage) throws Exception {
       stage.setTitle("Breakout!");
-      Image photo = new Image(getClass().getResource(bgImg).toURI().toString()); 
+      Image photo = new Image(getClass().getResource(BGIMG).toURI().toString()); 
       ImageView view = new ImageView(photo);
       root = new Group(view);
       Scene scene = new Scene(root, WIDTH, HEIGHT);
 
       createKeyHandler(root);
-      resetBallPaddleAndDestroyOld(root, false);
-      spawnNewBricks(root);
+      createBallPaddleAndDestroyOld(root, false);
+      createNewBricks(root);
       createSplashScreen(root, labelStart);
       createTopText(root);
 
@@ -108,42 +106,9 @@ public class Breakout extends Application {
       gameLoop.play();
    }
 
-   private void createSplashScreen(Group root, Label text) {
-      text.setStyle("-fx-text-fill: white; -fx-font: bold 20 \"serif\"; -fx-padding: 20 20 20 20; -fx-text-alignment: center");
-      splash.getChildren().addAll(text);
-      splash.setStyle("-fx-background-color: rgba(0, 100, 100, 0.5); -fx-background-radius: 10;");
-      splash.setMaxWidth(WIDTH - LABELPADD);
-      splash.setMaxHeight(HEIGHT - LABELPADD);
-      splash.setTranslateX(LABELX);
-      splash.setTranslateY(LABELY);
-      root.getChildren().add(splash);
-   }
-
-   private void createTopText(Group root) {
-      Label lives = new Label("Lives: " + playerLives);
-      Label level = new Label("Level: " + currentLevel);
-      Label score = new Label("Score: " + currentScore);
-      lives.setStyle("-fx-text-fill: white; -fx-font: bold 20 \"serif\"; -fx-padding: 10 10 10 10");
-      level.setStyle("-fx-text-fill: white; -fx-font: bold 20 \"serif\"; -fx-padding: 10 10 10 10");
-      score.setStyle("-fx-text-fill: white; -fx-font: bold 20 \"serif\"; -fx-padding: 10 10 10 10");
-      topText.add(lives, 0, 0);
-      topText.add(level, 1, 0);
-      topText.add(score, 2, 0);
-      topText.getColumnConstraints().add(new ColumnConstraints(WIDTH/3));
-      topText.getColumnConstraints().add(new ColumnConstraints(WIDTH/3));
-      topText.getColumnConstraints().add(new ColumnConstraints(WIDTH/3));
-      topText.setHalignment(lives, HPos.LEFT);
-      topText.setHalignment(level, HPos.CENTER);
-      topText.setHalignment(score, HPos.RIGHT);
-      root.getChildren().add(topText);
-   }
-
-   private void updateTopText(Group root) {
-      root.getChildren().remove(topText);
-      topText.getChildren().clear();
-      createTopText(root);
-   }
-
+   /*
+    * Game Loop Methods
+    */
    private void gameEvents(ActionEvent e) {
       if (roundHasStarted) {
          gameBall.update();
@@ -183,8 +148,8 @@ public class Breakout extends Application {
    private void checkBrickAndWinStatus() {
       if (brickList.isEmpty()) {
          playSound("win");
-         resetBallPaddleAndDestroyOld(root, true);
-         spawnNewBricks(root);
+         createBallPaddleAndDestroyOld(root, true);
+         createNewBricks(root);
          currentLevel++;
          createSplashScreen(root, labelGamewon);
          updateTopText(root);
@@ -209,13 +174,13 @@ public class Breakout extends Application {
 
    private void checkBallAndLivesStatus() {
       if (gameBall.isDead()) {
-         resetBallPaddleAndDestroyOld(root, true);
+         createBallPaddleAndDestroyOld(root, true);
          if (--playerLives == 0) {
             playSound("over");
             createSplashScreen(root, labelGameover);
-            spawnNewBricks(root);
             playerLives = 3;
             currentScore = 0;
+            createNewBricks(root);
          } else {
             playSound("died");
          }
@@ -223,36 +188,9 @@ public class Breakout extends Application {
       }
    }
 
-   private void spawnNewBricks(Group root) {
-      if (!brickList.isEmpty()) {
-         for (Brick currBrick : brickList) {
-            destroyBrick(currBrick);
-         }
-      }
-      createBricks(root);
-   }
-
-   private void resetBallPaddleAndDestroyOld(Group root, boolean destroyOld) {
-      if (destroyOld) {
-         root.getChildren().remove(gameBall);
-         root.getChildren().remove(gamePaddle);
-      }
-      createPaddle(root);
-      createNewBall(root);
-   }
-
-   // This method is necessary to avoid ConcurrentModificationException errors
-   // Solution adapted from : https://stackoverflow.com/q/16125311
-   private void destroyBrick(Brick brick) {
-      Platform.runLater(new Runnable() {
-         @Override
-         public void run() {
-            brickList.remove(brick);
-            root.getChildren().remove(brick);
-         }
-      });
-   }
-
+   /*
+    * Object Creation Methods
+    */
    // Create KeyEvent caller attached to an invisible rectangle object
    private void createKeyHandler(Group root) {
       final Rectangle keyboardNode = new Rectangle();
@@ -276,6 +214,15 @@ public class Breakout extends Application {
       root.getChildren().add(gamePaddle);
    }
 
+   private void createNewBricks(Group root) {
+      if (!brickList.isEmpty()) {
+         for (Brick currBrick : brickList) {
+            destroyBrick(currBrick);
+         }
+      }
+      createBricks(root);
+   }
+
    private void createBricks(Group root) {
       int xPos = BRICK_XOFFSET;
       int yPos = BRICK_YOFFSET;
@@ -293,6 +240,33 @@ public class Breakout extends Application {
       }
    }
 
+   /*
+    * Object Managment Methods
+    */
+   private void createBallPaddleAndDestroyOld(Group root, boolean destroyOld) {
+      if (destroyOld) {
+         root.getChildren().remove(gameBall);
+         root.getChildren().remove(gamePaddle);
+      }
+      createPaddle(root);
+      createNewBall(root);
+   }
+
+   // This method is necessary to avoid ConcurrentModificationException errors
+   // Solution adapted from : https://stackoverflow.com/q/16125311
+   private void destroyBrick(Brick brick) {
+      Platform.runLater(new Runnable() {
+         @Override
+         public void run() {
+            brickList.remove(brick);
+            root.getChildren().remove(brick);
+         }
+      });
+   }
+
+   /*
+    * Key Handling Methods
+    */
    private void keyPressed(KeyEvent key) {
       if (key.getCode() == KeyCode.SPACE && roundHasStarted == false) {
          playSound("paddle");
@@ -318,29 +292,71 @@ public class Breakout extends Application {
       }
    }
 
+   /*
+    * Sound Effect Methods
+    */
    private void playSound(String select) {
       URL path = null;
       AudioClip ac;
       switch (select) {
          case "paddle":
-            path = getClass().getResource(paddleHit);
+            path = getClass().getResource(PADDLEHIT);
             break;
          case "brick":
-            path = getClass().getResource(brickHit);
+            path = getClass().getResource(BRICKHIT);
             break;
          case "died":
-            path = getClass().getResource(died);
+            path = getClass().getResource(DIED);
             break;
          case "over":
-            path = getClass().getResource(gameover);
+            path = getClass().getResource(GAMEOVER);
             break;
          case "win":
-            path = getClass().getResource(gamewin);
+            path = getClass().getResource(GAMEWIN);
             break;
          default:
       }
       ac = new AudioClip(path.toString());
       ac.play();
+   }
+
+   /*
+    * Text Graphic Methods
+    */
+   private void createSplashScreen(Group root, Label text) {
+      text.setStyle("-fx-text-fill: white; -fx-font: bold 20 \"serif\"; -fx-padding: 20 20 20 20; -fx-text-alignment: center");
+      splash.getChildren().addAll(text);
+      splash.setStyle("-fx-background-color: rgba(0, 100, 100, 0.5); -fx-background-radius: 10;");
+      splash.setMaxWidth(WIDTH - LABELPADD);
+      splash.setMaxHeight(HEIGHT - LABELPADD);
+      splash.setTranslateX(LABELX);
+      splash.setTranslateY(LABELY);
+      root.getChildren().add(splash);
+   }
+
+   private void createTopText(Group root) {
+      Label lives = new Label("Lives: " + playerLives);
+      Label level = new Label("Level: " + currentLevel);
+      Label score = new Label("Score: " + currentScore);
+      lives.setStyle("-fx-text-fill: white; -fx-font: bold 20 \"serif\"; -fx-padding: 10 10 10 10");
+      level.setStyle("-fx-text-fill: white; -fx-font: bold 20 \"serif\"; -fx-padding: 10 10 10 10");
+      score.setStyle("-fx-text-fill: white; -fx-font: bold 20 \"serif\"; -fx-padding: 10 10 10 10");
+      topText.add(lives, 0, 0);
+      topText.add(level, 1, 0);
+      topText.add(score, 2, 0);
+      topText.getColumnConstraints().add(new ColumnConstraints(WIDTH/3));
+      topText.getColumnConstraints().add(new ColumnConstraints(WIDTH/3));
+      topText.getColumnConstraints().add(new ColumnConstraints(WIDTH/3));
+      topText.setHalignment(lives, HPos.LEFT);
+      topText.setHalignment(level, HPos.CENTER);
+      topText.setHalignment(score, HPos.RIGHT);
+      root.getChildren().add(topText);
+   }
+
+   private void updateTopText(Group root) {
+      root.getChildren().remove(topText);
+      topText.getChildren().clear();
+      createTopText(root);
    }
 
    public static void main(String[] args) {
